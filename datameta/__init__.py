13c5784ca51fb614b6d1d24e2090bab88e8585af
KerstenBreuer <kersten-breuer@outlook.com>
2021-02-23 16:51:59 +0000

    include openapi config

--- datameta/__init__.py
+++ datameta/__init__.py
@@ -1,5 +1,6 @@
 from pyramid.config import Configurator
 from pyramid_beaker import session_factory_from_settings
+import os
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
@@ -9,6 +10,11 @@ def main(global_config, **settings):
         session_factory = session_factory_from_settings(settings)
         config.set_session_factory(session_factory)
 
+        config.include("pyramid_openapi3")
+        config.pyramid_openapi3_spec(
+            os.path.join(os.path.dirname(__file__), "openapi.yaml")
+        )
+        config.pyramid_openapi3_add_explorer()
         config.include('.models')
         config.include('pyramid_chameleon')
         config.include('.routes')
