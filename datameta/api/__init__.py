558c03bc8a4f6e253bf9e9688f23448bc049770f
KerstenBreuer <kersten-breuer@outlook.com>
2021-02-24 17:17:20 +0000

    implement api key handling

--- datameta/api/__init__.py
+++ datameta/api/__init__.py
@@ -2,5 +2,4 @@ from pyramid.config import Configurator
 
 def includeme(config: Configurator) -> None:
     """Pyramid knob."""
-    config.add_route("metadatasets", "/api/metadatasets")
-    config.add_route("api_login", "/api/login")
+    config.add_route("apikeys", "/api/keys")
