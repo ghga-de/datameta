5d69eb7014cff02427ac6c9fed7b1aef58b14d3e
KerstenBreuer <kersten-breuer@outlook.com>
2021-02-24 11:15:47 +0000

    adapt include me

--- datameta/api/__init__.py
+++ datameta/api/__init__.py
@@ -3,4 +3,4 @@ from pyramid.config import Configurator
 
 def includeme(config: Configurator) -> None:
     """Pyramid knob."""
-    config.add_route("profile", "/api/profiles/{username}")
+    config.add_route("metadatasets", "/api/metadatasets")
