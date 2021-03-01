cbb788117c9881a6e17af6f36ad468660af2688f
KerstenBreuer <kersten-breuer@outlook.com>
2021-03-01 18:29:51 +0000

    update file model

--- datameta/models/db.py
+++ datameta/models/db.py
@@ -124,10 +124,11 @@ class File(Base):
     uuid             = Column(UUID(as_uuid=True), unique=True, default=uuid.uuid4, nullable=False)
     site_id          = Column(String(50), unique=True, nullable=False, index=True)
     name             = Column(Text, nullable=False)
-    storage_uri      = Column(String(2048), unique=True, nullable=True)
-    content_uploaded = Column(Boolean(create_constraint=False), nullable=False)
+    storage_uri      = Column(String(2048), unique=True, nullable=False)
     checksum         = Column(Text, nullable=False)
     filesize         = Column(BigInteger, nullable=True)
+    checksum_crypt   = Column(Text, nullable=True)
+    filesize_crypt   = Column(Text, nullable=True)
     user_id          = Column(Integer, ForeignKey('users.id'), nullable=False)
     group_id         = Column(Integer, ForeignKey('groups.id'), nullable=False)
     # Relationships
