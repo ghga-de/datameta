557c0b9b5870cde1c184182cd01f75e05aa0e437
Leon Kuchenbecker <leon.kuchenbecker@uni-tuebingen.de>
2021-03-02 16:13:14 +0100

    Fix pytest requirement

--- setup.py
+++ setup.py
@@ -47,12 +47,12 @@ requires = [
     'SQLAlchemy',
     'transaction',
     'zope.sqlalchemy',
-    'pyramid_openapi3'
+    'pyramid_openapi3',
+    'pytest >= 3.7.4',
 ]
 
 tests_require = [
     'WebTest >= 1.3.1',  # py3 compat
-    'pytest >= 3.7.4',
     'pytest-cov',
 ]
 
