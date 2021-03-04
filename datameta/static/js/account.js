3048c8534b69003d9eac1cf8665731c3d0fb9a40
Moritz Hahn <Moritz.Hahn@uni-tuebingen.de>
2021-03-04 14:44:54 +0000

    Incorporate API

--- datameta/static/js/account.js
+++ datameta/static/js/account.js
@@ -106,4 +106,131 @@ window.addEventListener("load", function() {
                 show_alert("An unknown error occurred. Please try again later.");
             });
     });
+
+    //Gets all the API keys and adds them to the table
+    fetch('/api/keys/' + data.get("uuid") + '/password',
+    {
+        method: 'get',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            
+        })
+    })
+    .then((response) => {
+        document.resp = response;
+        if (response.status == "204") {
+            view_success();
+            return;
+        } else {
+            if (response.status == "401") {
+                show_alert("You have to be logged in to perform this action.");
+            } else if (response.status == "403") {
+                show_alert("Wrong password.");
+            } else if(response.status == "400") {
+                show_alert("Your password has to have at least 10 Characters");   
+            } else {
+                show_alert("An unknown error occurred. Please try again later.");
+            }
+        }
+    })
+    .catch((error) => {
+        console.log(error);
+        show_alert("An unknown error occurred. Please try again later.");
+    });
+
+
+    // Adds an Api Key to the table
+    function addApiKey(id, label, expires) {
+        table = document.getElementById("table_apikeys");
+        row = table.insertRow();
+        row.id = id
+        cell1 = row.insertCell();
+        cell2 = row.insertCell();
+        cell3 = row.insertCell();
+        cell1.innerHTML = label;
+        cell2.innerHTML = expires;
+        cell3.innerHTML = '<button type="button" class="py-0 px-1 btn btn-sm btn-outline-danger"><i class="bi bi-trash-fill"></i></button>'
+        cell3.querySelector(".btn").addEventListener("click", function(event) {
+            deleteApiKey(id);
+        });
+    }
+
+    // Deletes an API key from the table and database
+    function deleteApiKey(id) {
+        //TODO DELTE key
+
+        //TODO: Only do this, if deletion was successfull
+        row = document.getElementById(id);
+        document.getElementById("tbody_apikeys").removeChild(row); 
+    }
+
+    // Clears the success and alert boxes
+    function clearApiAlerts() {
+        api_alert = document.getElementById("api_alert");
+        api_success = document.getElementById("api_success");
+        api_alert.innerHTML = "";
+        api_success.innerHTML = "";
+        api_alert.classList.remove("show");
+        api_success.classList.remove("show");
+    }
+
+    document.getElementById("api_keys_tab").addEventListener("click", function() {
+        clearApiAlerts();
+    }); 
+
+    document.getElementById("generate_api_key_button").addEventListener("click", function() {
+
+        // TODO: Open Form: Label && Expiration Date, get both from form
+        //
+
+        label = "test";
+
+        d = new Date();
+        d.setTime(d.getTime() + 24*60*60*1000);
+        expires = d.toUTCString();
+
+        // POST API key
+        fetch('/api/users/' + data.get("uuid") + '/password',
+            {
+                method: 'put',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    passwordChangeCredential: data.get("old_password"),
+                    newPassword: data.get("new_password")
+                })
+            })
+            .then((response) => {
+                document.resp = response;
+                if (response.status == "204") {
+                    view_success();
+                    return;
+                } else {
+                    if (response.status == "401") {
+                        show_alert("You have to be logged in to perform this action.");
+                    } else if (response.status == "403") {
+                        show_alert("Wrong password.");
+                    } else if(response.status == "400") {
+                        show_alert("Your password has to have at least 10 Characters");   
+                    } else {
+                        show_alert("An unknown error occurred. Please try again later.");
+                    }
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+                show_alert("An unknown error occurred. Please try again later.");
+        });
+
+        // Post Return Value to table && success box
+
+    });
+
+     
+
 });
