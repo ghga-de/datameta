60e64c6c31a1fb53dcafb817f36dceda45c232be
KerstenBreuer <kersten-breuer@outlook.com>
2021-03-11 16:30:21 +0000

    test get apikey list

--- tests/integration/__init__.py
+++ tests/integration/__init__.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import unittest
 
 from pyramid import testing
@@ -15,6 +16,8 @@ from datameta.models import (
 )
 from datameta.models.meta import Base
 from webtest import TestApp
+from dataclasses import dataclass
+from copy import deepcopy
 
 # get URL to test db from environment variable:
 db_url = os.getenv("SQLALCHEMY_TEST_URL")
@@ -31,57 +34,77 @@ with open(default_settings_json, "r") as json_:
 default_settings["sqlalchemy.url"] = db_url
 
 # read default user.json:
+@dataclass
+class UserFixture():
+    """A container for user information"""
+    email:str
+    password:str 
+    fullname:str
+    site_id:str
+    groupname:str
+    group_admin:bool
+    site_admin:bool
+    uuid:Optional[str] = None # will be set once
+                              # added to the DB
+
 default_users_json = os.path.join(
     os.path.dirname(__file__), 
     "fixtures", 
     "default_users.json"
 )
 with open(default_users_json, "r") as json_:
-    default_users = json.load(json_)
+    default_users = {
+        name: UserFixture(**user)
+        for name, user in json.load(json_).items()
+    }
 
 
 def create_user(
-    session_factory, 
-    email:str, 
-    password:str, 
-    fullname:str, 
-    site_id:str,
-    groupname:str,
-    group_admin:bool,
-    site_admin:bool
+    session_factory,
+    user:UserFixture 
 ):
     """Add a datameta user to the database"""
     with transaction.manager:
         session = get_tm_session(session_factory, transaction.manager)
         
         # check if group exists otherwise create it:
-        group = session.query(models.Group).filter(models.Group.name==groupname).one_or_none()
-        if not group:
-            group = models.Group(
-                name=groupname,
-                site_id=f"{groupname}_id" # ingore usual site id format
+        group_obj = session.query(models.Group).filter(models.Group.name==user.groupname).one_or_none()
+        if not group_obj:
+            group_obj = models.Group(
+                name=user.groupname,
+                site_id=f"{user.groupname}_id" # ingore usual site id format
             )
-            session.add(group)
+            session.add(group_obj)
             session.flush()
         
         # create user:
-        user = models.User(
-            site_id=site_id, # ingore usual site id format
+        user_obj = models.User(
+            site_id=user.site_id, # ingore usual site id format
             enabled=True,
-            email=email,
-            pwhash=security.hash_password(password),
-            fullname=fullname,
-            group=group,
-            group_admin=group_admin,
-            site_admin=site_admin
+            email=user.email,
+            pwhash=security.hash_password(user.password),
+            fullname=user.fullname,
+            group=group_obj,
+            group_admin=user.group_admin,
+            site_admin=user.site_admin
         )
-        session.add(user)
+        session.add(user_obj)
         session.flush()
 
-        return user
+        # return user updated with uuid:
+        user_updated = deepcopy(user)
+        user_updated.uuid = str(user_obj.uuid)
+        return user_updated
 
 class BaseIntegrationTest(unittest.TestCase):
     """Base TestCase to inherit from"""
+
+    state:dict # this slot can be used to store
+               # state when a test is broken down
+               # into multiple functions (steps),
+               # moreover, any initial parameters/fixtures
+               # should be defined here at the beginning
+               # of each test
 
     def initDb(self):
         # create database from scratch:
@@ -98,7 +121,7 @@ class BaseIntegrationTest(unittest.TestCase):
         
         # create default users:
         self.users = {
-            name: create_user(self.session_factory, **user)
+            name: create_user(self.session_factory, user)
             for name, user in default_users.items()
         }
 
--- tests/integration/test_auth.py
+++ tests/integration/test_auth.py
@@ -1,4 +1,5 @@
-from . import BaseIntegrationTest
+from . import BaseIntegrationTest, default_users
+from datameta import models
 
 class TestApiKeyUsageSenario(BaseIntegrationTest):
     """
@@ -8,11 +9,58 @@ class TestApiKeyUsageSenario(BaseIntegrationTest):
     def step_1_req_apikey(self):
         """Request ApiKey"""
         request_body = {
-            "email": "admin@admin.admin",
-            "password": "admin",
-            "label": "test"
+            "email": self.state["user"].email,
+            "password": self.state["user"].password,
+            "label": self.state["apikey_label"]
         }
-        response = self.testapp.post_json("/api/keys", request_body, status=200) 
-    
+        
+        response = self.testapp.post_json(
+            "/api/keys", 
+            params=request_body, 
+            status=200
+        ) 
+        
+        self.state["apikey_response"] = response.json
+
+    def step_2_list_apikeys(self):
+        """Get a list of all ApiKeys"""
+        # request params:
+        user_id = self.state["user"].uuid
+        token = self.state["apikey_response"]["token"] # previously created apikey 
+        request_headers = {
+            "Authorization": f"Bearer {token}"
+        }
+
+        response = self.testapp.get(
+            f"/api/users/{user_id}/keys",
+            headers=request_headers,
+            status=200
+        ) 
+
+        # check whether current response is consistent with the previous responce
+        # obtained when creating the api keys
+        keys_to_compare = ["apiKeyId", "label", "expiresAt"]
+        curr_response = {
+            key: value 
+            for key, value in response.json[0].items()
+            if key in keys_to_compare
+        }
+        prev_response = {
+            key: value 
+            for key, value in self.state["apikey_response"].items()
+            if key in keys_to_compare
+        }
+        assert curr_response == prev_response, (
+            "List of ApiKeys didn't contain the previously created apikey."
+        )
+
     def test_steps(self):
+        # set initial state:
+        self.state = {
+            "user": self.users["user"],
+            "apikey_label": "test_key",
+            "apikey_response": None # slot to store the apikey response
+        }
+
+        # execute all test steps
         self._test_all_steps()
