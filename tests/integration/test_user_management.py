b7d90eeab7203634ab8c0dd28125e1e403291e05
Moritz Hahn <Moritz.Hahn@uni-tuebingen.de>
2021-03-26 08:46:54 +0100

    Does not work, due to session problems

--- tests/integration/test_user_management.py
+++ tests/integration/test_user_management.py
@@ -1,55 +1,146 @@
-    """Test user management for the API
-        +: Changes that should work
-        -: Changes that should not work
-    """
+"""Tests user management for the API
+    +: Changes that should work
+    -: Changes that should not work
+"""
 
-    """Test 1: Namechange
-    a) Normal user:
-        + change name yourself
-        - change somebody else
-    b) Group Admin:
-        + change somebody in group
-        - change somebody in other group
-    c) Site Admin:
-        + change somebody not in group 
-    """
+"""Test 1: Namechange
+a) user_a:
+    + change name of user_a
+    - change name of user_c
+b) group_x_admin:
+    + change name of user_a
+    - change name of user_c
+c) admin:
+    + change name of user_a
+"""
 
-    """Test 2: Group Change
-    a) Normal user:
-        - change your group
-    b) Group Admin:
-        - change your group
-        - change group from somebody in your group
-        - change group from somebody not in your group to your group
-    c) Site Admin:
-        + change your own group
-        + change somebody elses group
-    """
+"""Test 2: Group Change
+a) user_a:
+    - change group of user_a
+b) group_x_admin:
+    - change group of group_x_admin
+    - change group of user_a
+    - change group of user_c to group_x
+c) admin:
+    + change group of admin
+    + change group of user_a
+"""
 
-    """Test 3: Admin settings change
-    a) Normal user:
-        - try to make yourself group/site admin
-        - try to take away somebody elses admin permissions
-    b) Group Admin:
-        + Make user in your group Admin
-        + Demote this user back
-        - make yourself site admin
-        - make another user site admin
-        - demote a site admin
-        - demote a site admin from beeing group admin in your group
-    c) Site Admin:
-        + Promote user to group & site admin
-        + Demote user back
-    """
+"""Test 3: Admin settings change
+a) user_a:
+    - make user_a admin
+    - remove admin status of admin
+b) group_x_admin:
+    + make user_a group_admin
+    + remove user_a as group_admin
+    - make group_x_admin site admin
+    - make user_a site admin
+    - remove admin status of admin
+c) admin:
+    + make user_a group & site admin 
+    + remove admin status of user_a
+"""
 
-    """Test 4: Enabled settings change:
-    Note, that the enabled setting currently does not do anything. Once this changes, these tests have to be expanded
-    a) Normal user:
-        - disable/enabled yourself
-    b) Group Admin:
-        + disable/enable user in your group
-        - disable/enable user in your group which is site admin
-        - disable/enable user in another group
-    c) Site Admin:
-        + disable/enable user
-    """
+"""Test 4: Enabled settings change:
+Note, that the enabled setting currently does not do anything. Once this changes, these tests have to be expanded
+a) user_a:
+    - disable/enable user_a
+b) group_x_admin:
+    + disable/enable user_a
+    - (admin: make user_b site admin) disable/enable user_b
+    - disable/enable user_c
+c) admin:
+    + disable/enable user_b
+"""
+
+from . import BaseIntegrationTest, default_users
+from .utils import get_auth_headers
+
+from datameta import models
+from datameta.api import base_url
+
+class TestUserManagement(BaseIntegrationTest):
+
+    def step_1(self, status:int=200):
+        """user_a: change name of user_a"""
+        request_body = {
+            "email": self.state["user_a"].email,
+            "password": self.state["user_a"].password,
+            "name": "changed_by_user_a"
+        }
+
+        response = self.testapp.put_json(
+            base_url + "/user",
+            params=request_body,
+            status=status
+        )
+
+    def step_2(self, status:int=403):
+        """user_a: change name of user_c"""
+        request_body = {
+            "email": self.state["user_a"].email,
+            "password": self.state["user_a"].password,
+            "name": "changed_by_user_a"
+        }
+
+        response = self.testapp.put_json(
+            base_url + "/user/" + self.state["user_c"].uuid,
+            params=request_body,
+            status=status
+        )
+
+    def step_3(self, status:int=200):
+        """group_x_admin: change name of user_a"""
+        request_body = {
+            "email": self.state["group_x_admin"].email,
+            "password": self.state["group_x_admin"].password,
+            "name": "changed_by_group_x_admin"
+        }
+
+        response = self.testapp.put_json(
+            base_url + "/user/" + self.state["user_a"].uuid,
+            params=request_body,
+            status=status
+        )
+
+    def step_4(self, status:int=403):
+        """group_x_admin: change name of user_c"""
+        request_body = {
+            "email": self.state["group_x_admin"].email,
+            "password": self.state["group_x_admin"].password,
+            "name": "changed_by_group_x_admin"
+        }
+
+        response = self.testapp.put_json(
+            base_url + "/user/" + self.state["user_c"].uuid,
+            params=request_body,
+            status=status
+        )
+
+    def step_5(self, status:int=200):
+        """admin: change name of user_a"""
+        request_body = {
+            "email": self.state["admin"].email,
+            "password": self.state["admin"].password,
+            "name": "changed_by_admin"
+        }
+
+        response = self.testapp.put_json(
+            base_url + "/user/" + self.state["user_a"].uuid,
+            params=request_body,
+            status=status
+        )
+
+    def test_steps(self):
+        # set initial state:
+        self.state = {
+            "user_a": self.users["user_a"],
+            "user_b": self.users["user_b"],
+            "user_c": self.users["user_c"],
+            "group_x_admin": self.users["group_x_admin"],
+            "group_y_admin": self.users["group_y_admin"],
+            "admin": self.users["admin"],
+        }
+
+        # execute all test steps
+        self._test_all_steps()
