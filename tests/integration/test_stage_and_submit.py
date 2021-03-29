c44f1f3dc01ac2725cd217b2e1b5e970690073f7
KerstenBreuer <kersten-breuer@outlook.com>
2021-03-29 08:31:56 +0000

    add test for delete metadata

--- tests/integration/test_stage_and_submit.py
+++ tests/integration/test_stage_and_submit.py
@@ -61,8 +61,8 @@ class TestStageAndSubmitSenario(BaseIntegrationTest):
             # as request record (values might have changed
             # due to backend side processing):
             assert metadata_record.keys() == response.json["record"].keys()
-
-        return response.json
+            
+            return response.json
 
     
     def get_metadata(
@@ -86,26 +86,38 @@ class TestStageAndSubmitSenario(BaseIntegrationTest):
             # as expected record (values might have changed
             # due to backend side processing):
             assert expected_record.keys() == response.json["record"].keys()
-        
-        return response.json
 
+            return response.json
 
-    def test_steps(self):
-        # get fixtures:
-        user = self.users["user_a"]
-        metadata_records = self.metadata_records
+    def delete_metadata(
+        self,
+        token:str,
+        metadataset_id:str,
+        status:int=204
+    ):
+        # request params:
+        request_headers = get_auth_headers(token)
 
+        response = self.testapp.delete(
+            base_url + f"/metadatasets/{metadataset_id}",
+            headers=request_headers,
+            status=status
+        )
+
+    def test_main_submission_senario(self):
+        """Tests the standard usage senario for staging and 
+        submitting files and metadata."""
         # get token
-        user_session = self.post_key(user=user)
+        user_session = self.post_key(user=self.users["user_a"])
         token = user_session["token"]
 
-        # post metadata:
+        # post metadataset:
         metadataset_ids = [
             self.post_metadata(
                 token=token, 
                 metadata_record=rec
             )["id"]["uuid"]
-            for rec in metadata_records
+            for rec in self.metadata_records
         ]
         
 
@@ -114,7 +126,37 @@ class TestStageAndSubmitSenario(BaseIntegrationTest):
             self.get_metadata(
                 token=token, 
                 metadataset_id=m_id,
-                expected_record=metadata_records[idx]
+                expected_record=self.metadata_records[idx]
             )
             for idx, m_id in enumerate(metadataset_ids)
         ]
+
+
+    def test_stage_and_delete_metadata(self):
+        """Test whether staged metadatasets can be deleted
+        after staging"""
+        metadata_record = self.metadata_records[0]
+
+        # get token
+        user_session = self.post_key(user=self.users["user_a"])
+        token = user_session["token"]
+
+        # post metadataset:
+        metadataset_id = self.post_metadata(
+            token=token, 
+            metadata_record=metadata_record
+        )["id"]["uuid"]
+        
+        # delete metdataset:
+        _ = self.delete_metadata(
+            token, 
+            metadataset_id
+        )
+
+        # get metadatasets and compare to original records:
+        _ = self.get_metadata(
+            token=token, 
+            metadataset_id=metadataset_id,
+            expected_record=metadata_record,
+            status=404
+        )
