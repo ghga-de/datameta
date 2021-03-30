2fc9bdd1d5ff73b49fad64a280dcd0886b523bca
Leon Kuchenbecker <leon.kuchenbecker@uni-tuebingen.de>
2021-03-30 22:19:46 +0200

    Remove legacy code

--- datameta/linting.py
+++ datameta/linting.py
@@ -21,85 +21,6 @@ from collections import Counter
 import re
 import datetime
 
-def lint_pending_msets(request, user, mset_ids = None):
-    """Performs linting on the pending metadatasets for a given user. Optionally filter those
-    metadatasets for only those specified in the ID list."""
-    db = request.dbsession
-
-    # Query metadata fields
-    mdats = db.query(MetaDatum).order_by(MetaDatum.order).all()
-    mdat_names = [ mdat.name for mdat in mdats ]
-    mdat_names_files = [ mdat.name for mdat in mdats if mdat.isfile ]
-
-    # Obtain all pending metadatasets
-    if mset_ids is None:
-        mdatasets = db.query(MetaDataSet).filter(and_(
-            MetaDataSet.submission_id==None,
-            MetaDataSet.user_id==user.id))
-    # Or obtain all pending metadatasets that match the specified mset ids
-    else:
-        mdatasets = db.query(MetaDataSet).filter(and_(
-            MetaDataSet.submission_id==None,
-            MetaDataSet.user_id==user.id,
-            MetaDataSet.id.in_(mset_ids)))
-
-    # Create result structure
-    linting_report = { mdataset.id : [] for mdataset in mdatasets }
-
-    # Obtain all corresponding metadata records
-    mdatrecs = [ mdatrec for mdataset in mdatasets for mdatrec in mdataset.metadatumrecords ]
-
-    # Keep track of file names
-    file_names        = [ mdatrec.value for mdatrec in mdatrecs if mdatrec.metadatum.isfile ]
-    file_names_red    = [ file_name for file_name, count in Counter(file_names).items() if count > 1 ]
-    # Keep track of already uploaded files...
-    uploaded_files = db.query(File).filter(and_(File.user_id==user.id, File.metadatumrecord == None))
-    uploaded_files = { file.name : file for file in uploaded_files }
-    # ... and their association with file names
-    file_pairs = []
-    for mdatrec in mdatrecs:
-        metadatum = mdatrec.metadatum
-        mdset_id  = mdatrec.metadataset.id
-        value     = mdatrec.value
-        # If this metadatum is mandatory or has been specified "voluntarily" it
-        # has to undergo validation
-        if metadatum.mandatory or value != "":
-            # Count file name occurrences and report duplicates
-            if metadatum.isfile and value in file_names_red:
-                linting_report[mdset_id].append({
-                    'field' : metadatum.name,
-                    'type' : 'custom',
-                    'error' : f"Filename '{value}' is specified multiple times in the sample sheet."
-                    })
-            # Check for files that have not yet been uploaded
-            if metadatum.isfile and value not in uploaded_files.keys():
-                linting_report[mdset_id].append({
-                    'field' : metadatum.name,
-                    'type' : 'nofile',
-                    })
-            elif metadatum.isfile:
-                file_pairs.append((mdatrec, uploaded_files[value]))
-            # Check if the regexp pattern matches
-            if metadatum.regexp and re.match(metadatum.regexp, value) is None:
-                linting_report[mdset_id].append({
-                    'field' : metadatum.name,
-                    'type' : 'custom',
-                    'error' : metadatum.short_description
-                    })
-            # Check if the datetime was correctly parsed or specified at all
-            if metadatum.datetimefmt and value=="":
-                linting_report[mdset_id].append({
-                    'field' : metadatum.name,
-                    'type' : 'custom',
-                    'error' : "The field was either empty or it could not be parsed as a valid date / time"
-                    })
-
-    # Return those metadatasets that passed linting, the record-file associations and the linting
-    # report for the failed records
-    good_sets = [ mdataset for mdataset in mdatasets if not linting_report[mdataset.id] ]
-    return good_sets, file_pairs, linting_report
-
-
 def validate_metadataset_record(
     request:Request,
     record:dict, 
@@ -116,9 +37,9 @@ def validate_metadataset_record(
     db = request.dbsession
     mdats_query = db.query(MetaDatum).order_by(MetaDatum.order).all()
     mdats = { mdat.name: mdat for mdat in mdats_query }
-    
+
     for name, mdat in mdats.items():
-        
+
         # check if mdat is present in record dict
         # if not and mdat is mandatory, throw an error:
         if name not in record:
@@ -128,7 +49,7 @@ def validate_metadataset_record(
                     "field": name
                 })
             continue
-        
+
         value = record[name]
 
         # if value is none but mandatory,
@@ -157,7 +78,7 @@ def validate_metadataset_record(
                 "field": name
             })
             continue
-        
+
         # check if datetime formats are matched
         if mdat.datetimefmt:
             try:
@@ -174,8 +95,8 @@ def validate_metadataset_record(
                     "message": "The field could not be parsed as a valid date / time",
                     "field": name
                 })
-                continue   
-    
+                continue
+
     # check if any of the record fields has no corresponding MetaDatum object:
     mdats_set = set(mdats.keys())
     record_set = set(record.keys())
@@ -185,7 +106,7 @@ def validate_metadataset_record(
                 "message": "The field was not expected.",
                 "field": field
             })
-            
+
     # return the error messages
     # or raise validation errors
     if return_err_message:
@@ -193,7 +114,7 @@ def validate_metadataset_record(
     elif len(errors) > 0:
         messages = [err["message"] for err in errors]
         fields = [
-            err["field"] if "field" in err else None 
+            err["field"] if "field" in err else None
             for err in errors
         ]
         raise get_validation_error(messages, fields)
--- datameta/views/submit.py
+++ datameta/views/submit.py
@@ -13,81 +13,8 @@
 # limitations under the License.
 
 from pyramid.view import view_config
-from pyramid.httpexceptions import HTTPFound
 
-from sqlalchemy.exc import DBAPIError
-from sqlalchemy import and_, or_
-
-from ..models import MetaDataSet, File, MetaDatum, MetaDatumRecord, Submission
 from .. import security
-
-import uuid
-import os
-import shutil
-import logging
-import hashlib
-
-import webob
-import datetime
-
-import logging
-log = logging.getLogger(__name__)
-
-from ..samplesheet import import_samplesheet, SampleSheetReadError
-from .. import storage, linting, siteid
-
-class FileDeleteError(RuntimeError):
-    pass
-
-def submit_samplesheet(request, user):
-    """Handle a samplesheet submission request"""
-    success = []
-    errors = []
-
-    for file_obj in request.POST.getall('files[]'):
-        if  isinstance(file_obj, webob.compat.cgi_FieldStorage):
-            input_file = file_obj.file
-
-            input_file.seek(0)
-            try:
-                n_added = import_samplesheet(request, input_file, user)
-                success.append({
-                    'filename' : file_obj.filename,
-                    'n_added' : n_added
-                    })
-            except SampleSheetReadError as e:
-                log.warning(f"Sample sheet '{file_obj.filename}' could not be read: {e}")
-                errors.append({
-                    'filename' : file_obj.filename,
-                    'reason' : str(e)
-                    })
-    return {
-            'success' : success,
-            'errors' : errors
-            }
-
-def sizeof_fmt(num, suffix='B'):
-    if num is None:
-        return "unknown";
-    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
-        if abs(num) < 1024.0:
-            return "%3.2f %s%s" % (num, unit, suffix)
-        num /= 1024.0
-    return "%.2f %s%s" % (num, 'Yi', suffix)
-
-
-def formatted_mrec_value(mrec):
-    if mrec.metadatum.datetimefmt is not None:
-        try:
-            return datetime.datetime.fromisoformat(mrec.value).strftime(mrec.metadatum.datetimefmt)
-        except ValueError:
-            pass
-    return mrec.value
-
-
-####################################################################################################
-# VIEWS
-####################################################################################################
 
 @view_config(route_name='submit', renderer='../templates/submit.pt')
 def v_submit(request):
