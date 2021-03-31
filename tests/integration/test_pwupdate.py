805aaeaf1ae28889c67266c7e2e7d3b38ecc489b
Christian Schudoma <cschu1981@gmail.com>
2021-03-31 12:30:45 +0000

    refactored groupname change tests to accommodate auth/default token changes (57c08e8, 91e25e1)\nadded pw-reset token tests

--- tests/integration/test_pwupdate.py
+++ tests/integration/test_pwupdate.py
@@ -1,16 +1,22 @@
 """Testing password update via API request
 """
+import time
+from datetime import datetime, timedelta
 
 from . import BaseIntegrationTest
-from .utils import get_auth_headers
 from datameta.api import base_url
+from .utils import create_pwtoken
+
 
 class TestPasswordUpdate(BaseIntegrationTest):
 
     def test_successful_own_password_update(self, status:int=204):
+        """Testing successful (self-)password change.
+
+        Expected Response:
+            HTTP 204
+        """
         user = self.users["user_a"]
-        token = self.get_api_key("user_a", base_url)
-        request_headers = get_auth_headers(token)
 
         request_body = {
             "passwordChangeCredential": user.password,
@@ -19,16 +25,40 @@ class TestPasswordUpdate(BaseIntegrationTest):
 
         response = self.testapp.put_json(
             base_url + f"/users/{user.site_id}/password",
-            headers=request_headers,
+            headers=user.auth.header,
             params=request_body,
             status=status
         )
 
-    def test_failure_other_password_update(self, status:int=403):
-        attacker, victim = self.users["user_a"], self.users["user_b"]
-        token = self.get_api_key("user_a", base_url)
+    def test_failure_invalid_password_update(self, status:int=400):
+        """Testing unsuccessful (self-)password change with invalid password.
 
-        request_headers = get_auth_headers(token)
+        Expected Response:
+            HTTP 400
+        """
+        user = self.users["user_a"]
+
+        request_body = {
+            "passwordChangeCredential": user.password,
+            "newPassword": "*meep*"
+        }
+
+        response = self.testapp.put_json(
+            base_url + f"/users/{user.site_id}/password",
+            headers=user.auth.header,
+            params=request_body,
+            status=status
+        )
+
+
+    def test_failure_other_password_update(self, status:int=403):
+        """Testing unsuccessful password change for other user.
+
+        Expected Response:
+            HTTP 403
+        """
+        attacker, victim = self.users["user_a"], self.users["user_b"]
+
         request_body = {
             "passwordChangeCredential": attacker.password,
             "newPassword": "012345678910"
@@ -36,29 +66,110 @@ class TestPasswordUpdate(BaseIntegrationTest):
 
         response = self.testapp.put_json(
             base_url + f"/users/{victim.site_id}/password",
-            headers = request_headers,
+            headers = attacker.auth.header,
             params = request_body,
             status=status
         )
 
-    def test_failure_invalid_reset_token(self, status:int=404):        
-        token = self.get_api_key("user_a", base_url)
+    def test_failure_invalid_user_password_update(self, status:int=403):
+        """Testing unsuccessful (self-)password change for invalid/non-existing user
 
-        request_headers = get_auth_headers(token)
+        Expected Response:
+            HTTP 403
+        """
+        attacker = self.users["user_a"]
+
         request_body = {
-            "passwordChangeCredential": "Cz628nCFsstFaHsfelHnbz9JvIjnsRy0n_jt9HuBC-cUKz6R4Su0yQ",
+            "passwordChangeCredential": attacker.password,
+            "newPassword": "012345678910"
+        }
+
+        response = self.testapp.put_json(
+            base_url + f"/users/nihilist/password",
+            headers = attacker.auth.header,
+            params = request_body,
+            status=status
+        )
+
+    def test_failure_invalid_reset_token(self, status:int=404):
+        """Testing unsuccessful (self-)password change with invalid token.
+
+        Expected Response:
+            HTTP 404
+        """
+        user = self.users["user_a"]
+
+        request_body = {
+            "passwordChangeCredential": "this_token_does_not_exist_for_sure",
             "newPassword": "012345678910"
         }
 
         response = self.testapp.put_json(
             base_url + f"/users/0/password",
-            headers = request_headers,
+            headers = user.auth.header,
             params = request_body,
             status = status
         )
 
     def test_failure_expired_reset_token(self, status:int=410):
-        token = self.get_api_key("user_a", base_url) #, expires="2021-01-18") # <- that is an expired api key, not a pw-reset key >:(
+        """Testing unsuccessful (self-)password change with expired token.
 
-        request_headers = get_auth_headers(token)
-        assert True
+        Expected Response:
+            HTTP 410
+        """
+        user = self.users["user_a"]
+        pwtoken = create_pwtoken(self.session_factory, user, expires=datetime.now() + timedelta(seconds=1))
+        time.sleep(5)
+
+        request_body = {
+            "passwordChangeCredential": pwtoken,
+            "newPassword": "012345678910"
+        }
+
+        response = self.testapp.put_json(
+            base_url + f"/users/0/password",
+            headers = user.auth.header,
+            params = request_body,
+            status = status
+        )
+
+    def test_successful_own_password_reset(self, status:int=204):
+        """Testing successful (self-)password change via reset token.
+
+        Expected Response:
+            HTTP 204
+        """
+        user = self.users["user_b"]
+        pwtoken = create_pwtoken(self.session_factory, user)
+
+        request_body = {
+            "passwordChangeCredential": pwtoken,
+            "newPassword": "012345678910"
+        }
+
+        response = self.testapp.put_json(
+            base_url + f"/users/0/password",
+            headers=user.auth.header,
+            params=request_body,
+            status=status
+        )
+
+    def test_failure_own_password_update_expired_apitoken(self, status:int=401):
+        """Testing unsuccessful (self-)password change with expired api token.
+
+        Expected Response:
+            HTTP 401
+        """
+        user = self.users["user_a"]
+
+        request_body = {
+            "passwordChangeCredential": user.password,
+            "newPassword": "012345678910"
+        }
+
+        response = self.testapp.put_json(
+            base_url + f"/users/{user.site_id}/password",
+            headers=user.expired_auth.header,
+            params=request_body,
+            status=status
+        )
