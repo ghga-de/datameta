fba838b98d6c0af857967fd3b12ed23cad03172f
Leon Kuchenbecker <leon.kuchenbecker@uni-tuebingen.de>
2021-04-20 18:52:17 +0200

    Bump Version 0.9.1

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ tests_require = [
 
 setup(
     name                   = 'datameta',
-    version                = '0.9.0',
+    version                = '0.9.1',
     description            = 'DataMeta - submission server for data and associated metadata',
     long_description       = README + '\n\n' + CHANGES,
     author                 = 'Leon Kuchenbecker',
