b0c2af4a8c6c1d7dc60dbd2d7d1456ecf22b10b6
root <cschu1981@gmail.com>
2021-04-21 14:59:31 +0000

    renamed `is_authorized` functions

--- datameta/api/submissions.py
+++ datameta/api/submissions.py
@@ -47,13 +47,13 @@ def validate_submission_access(db, db_files, db_msets, auth_user):
     Raises:
         400 HTTPBadRequest
     """
-    auth_f = authz.is_authorized_file_submission
+    auth_f = authz.file_submission
     # Collect missing files
     val_errors = [ ({ 'site' : file_id, 'uuid' : file_id }, None, "Not found") for file_id, db_file in db_files.items() if db_file is None ]
     # Collect authorization issues
     val_errors += [ ({ 'site' : file_id, 'uuid' : file_id }, None, "Access denied") for file_id, db_file in db_files.items() if db_file is not None and not auth_f(auth_user, db_file) ]
 
-    auth_f = authz.is_authorized_mds_submission
+    auth_f = authz.mds_submission
     # Collect missing metadatasets
     val_errors += [ ({ 'site' : mset_id, 'uuid' : mset_id }, None, "Not found") for mset_id, db_mset in db_msets.items() if db_mset is None ]
     # Collect authorization issues
