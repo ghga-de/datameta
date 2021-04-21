8ee81c975f155a940e62064161c69d17c9651cd9
Leon Kuchenbecker <leon.kuchenbecker@uni-tuebingen.de>
2021-04-21 13:27:02 +0200

    Refactor TAL values, redesign footer, add GET:/server

--- datameta/__init__.py
+++ datameta/__init__.py
@@ -17,6 +17,10 @@ from pyramid_beaker import session_factory_from_settings
 import os
 from . import api
 
+from pkg_resources import get_distribution
+
+__version__ = get_distribution('datameta').version
+
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
