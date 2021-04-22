03f6dcae45aac8a737fc89c702b1cabe79ca3376
Leon Kuchenbecker <leon.kuchenbecker@uni-tuebingen.de>
2021-04-22 17:30:02 +0200

    Remove re-defined view function

--- datameta/views/account.py
+++ datameta/views/account.py
@@ -24,8 +24,3 @@ from .. import security
 def my_view(request):
     security.revalidate_user_or_login(request)
     return {}
-
-@view_config(route_name='account', renderer='../templates/account.pt')
-def my_view(request):
-    user = security.revalidate_user_or_login(request)
-    return {'user' : { 'uuid' : user.uuid } }
