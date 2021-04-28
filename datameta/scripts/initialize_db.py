e2891909fbbc91f6e675d1b2a1e4609775ff90fb
Leon Kuchenbecker <leon.kuchenbecker@uni-tuebingen.de>
2021-04-28 13:25:13 +0200

    Add 'head_html' appsetting in init script

--- datameta/scripts/initialize_db.py
+++ datameta/scripts/initialize_db.py
@@ -207,6 +207,9 @@ def create_default_site_settings(db):
         key='logo_html',
         str_value = '<p class="h4 my-0 me-md-auto fw-normal" style="font-family: \'Fira Sans\', sans-serif;"><a href="/" class="link-bare"><span style="color:#ffca2c">D</span>ata<span style="color:#ffca2c">M</span>eta</a></p>'
         ))
+    db.add(ApplicationSettings(
+        key='head_html',
+        ))
 
 def main(argv=sys.argv):
     args = parse_args(argv)
