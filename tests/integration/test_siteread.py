c93383b1a6cbad638b790813ece3460d526ea527
KerstenBreuer <kersten-breuer@outlook.com>
2021-05-05 07:20:42 +0000

    iterate over files and msets in site read test

--- tests/integration/test_siteread.py
+++ tests/integration/test_siteread.py
@@ -11,20 +11,22 @@ class SiteReadTest(BaseIntegrationTest):
         # TypeError: 'NoneType' object is not iterable
         # /usr/local/lib/python3.8/site-packages/openapi_core/unmarshalling/schemas/unmarshallers.py:149: TypeError
         file_ids = [site_id for site_id, f in self.default_files.items() if not f.submitted]
-        user = self.default_users["admin"]
+        user = self.default_users["group_z_site_read"]
 
-        response = self.testapp.get(
-            f"{base_url}/files/{file_ids[0]}",
-            headers=user.auth.header,
-            status=403
-        )
+        for file_id in file_ids:
+            response = self.testapp.get(
+                f"{base_url}/files/{file_id}",
+                headers=user.auth.header,
+                status=403
+            )
 
     def test_access_staged_metadataset(self):
         mds_ids = [site_id for site_id, mds in self.default_metadatasets.items() if not mds.submitted]
-        user = self.default_users["admin"]
+        user = self.default_users["group_z_site_read"]
 
-        response = self.testapp.get(
-            f"{base_url}/metadatasets/{mds_ids[0]}",
-            headers=user.auth.header,
-            status=403
-        )
+        for mds_id in mds_ids:
+            response = self.testapp.get(
+                f"{base_url}/metadatasets/{mds_id}",
+                headers=user.auth.header,
+                status=403
+            )
