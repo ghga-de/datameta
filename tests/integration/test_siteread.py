75e824d85ffdd501fa27e691750e4b72ef5ee300
KerstenBreuer <kersten-breuer@outlook.com>
2021-05-05 07:22:03 +0000

    cleanup

--- tests/integration/test_siteread.py
+++ tests/integration/test_siteread.py
@@ -7,9 +7,6 @@ from datameta.api import base_url
 
 class SiteReadTest(BaseIntegrationTest):
     def xtest_access_staged_file(self):
-        # this still throws 
-        # TypeError: 'NoneType' object is not iterable
-        # /usr/local/lib/python3.8/site-packages/openapi_core/unmarshalling/schemas/unmarshallers.py:149: TypeError
         file_ids = [site_id for site_id, f in self.default_files.items() if not f.submitted]
         user = self.default_users["group_z_site_read"]
 
