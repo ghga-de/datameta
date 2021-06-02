4d1c0cbf2c36ce6125dad4b767658b707018dfab
root <cschu1981@gmail.com>
2021-06-02 09:23:41 +0000

    addressed .flake8 errors

--- tests/integration/test_service_execution.py
+++ tests/integration/test_service_execution.py
@@ -21,6 +21,7 @@ from datameta import models
 
 from . import BaseIntegrationTest
 
+
 class ServiceExecutionTest(BaseIntegrationTest):
     def setUp(self):
         super().setUp()
@@ -36,10 +37,8 @@ class ServiceExecutionTest(BaseIntegrationTest):
         self.fixture_manager.copy_files_to_storage()
         self.fixture_manager.populate_metadatasets()
 
-        
-    
     @parameterized.expand([
-        ("unauthorized_no_user", "",  "313", "x", 401),
+        ("unauthorized_no_user", "", "313", "x", 401),
         ("forbidden_no_service_user", "admin", "service_0", "mset_a", 403),
         ("forbidden_service_already_executed", "service_user_0", "service_0", "mset_a_sexec", 403),
         ("success", "service_user_0", "service_0", "mset_a", 200),
@@ -72,7 +71,7 @@ class ServiceExecutionTest(BaseIntegrationTest):
 
         request_body = test_data.get(testname, test_data["success"])
 
-        response = self.testapp.post_json(
+        self.testapp.post_json(
             url       = f"{base_url}/service-execution/{service_id}/{mset_id}",
             headers   = auth_headers,
             status    = expected_status,
@@ -80,10 +79,10 @@ class ServiceExecutionTest(BaseIntegrationTest):
         )
 
         if expected_status == 200:
-          mset_after = self.fixture_manager.get_fixture_db("metadatasets", mset_id, joinedload(models.MetaDataSet.service_executions).joinedload(models.ServiceExecution.service))
-          assert service_id in (sexec.service.site_id for sexec in mset_after.service_executions), f"Service {service_id} was not executed."
+            mset_after = self.fixture_manager.get_fixture_db("metadatasets", mset_id, joinedload(models.MetaDataSet.service_executions).joinedload(models.ServiceExecution.service))
+            assert service_id in (sexec.service.site_id for sexec in mset_after.service_executions), f"Service {service_id} was not executed."
 
-          mset_after = self.fixture_manager.get_fixture_db("metadatasets", mset_id, joinedload(models.MetaDataSet.metadatumrecords).joinedload(models.MetaDatumRecord.metadatum))
-          mdrecords = {mdr.metadatum.name: mdr.value for mdr in mset_after.metadatumrecords}
-          for key, value in request_body.get("record", dict()).items():
-              assert mdrecords.get(key) == str(value), f"Metadata was not set as expected {key}: {mdrecords.get(key)}, expected: {value}"
+            mset_after = self.fixture_manager.get_fixture_db("metadatasets", mset_id, joinedload(models.MetaDataSet.metadatumrecords).joinedload(models.MetaDatumRecord.metadatum))
+            mdrecords = {mdr.metadatum.name: mdr.value for mdr in mset_after.metadatumrecords}
+            for key, value in request_body.get("record", dict()).items():
+                assert mdrecords.get(key) == str(value), f"Metadata was not set as expected {key}: {mdrecords.get(key)}, expected: {value}"
