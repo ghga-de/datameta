b58787d9ea16caca9cd2968cc88a4d99b413d20e
Christian Schudoma <cschu1981@gmail.com>
2021-10-29 16:52:01 +0200

    version bump -> 1.1.0

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ tests_require = [
 
 setup(
     name                   = 'datameta',
-    version                = '1.0.5',
+    version                = '1.1.0',
     description            = 'DataMeta - submission server for data and associated metadata',
     long_description       = README + '\n\n' + CHANGES,
     author                 = 'Leon Kuchenbecker',
