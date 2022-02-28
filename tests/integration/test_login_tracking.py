95833ebf0ecfbb4f336ad6a7b0aeb1be9ed7e4a0
Christian Schudoma <cschu1981@gmail.com>
2022-02-28 16:58:11 +0100

    added test for apikey-based login tracking

--- tests/integration/test_login_tracking.py
+++ tests/integration/test_login_tracking.py
@@ -17,6 +17,8 @@
 from parameterized import parameterized
 
 from . import BaseIntegrationTest
+from datameta.api import base_url
+from .utils import get_auth_header
 
 
 class TestLoginTracking(BaseIntegrationTest):
@@ -25,13 +27,14 @@ class TestLoginTracking(BaseIntegrationTest):
         super().setUp()
         self.fixture_manager.load_fixtureset('groups')
         self.fixture_manager.load_fixtureset('users')
+        self.fixture_manager.load_fixtureset('apikeys')
 
     @parameterized.expand([
         # TEST_NAME                    EXEC_USER   SUCCESS?  EXPECTED_OUTCOME
         ("login_failure_with_block"  , "user_a"  , False   , (302, "login", False)),
         ("login_success_after_n-1"   , "user_a"  , True    , (302, "home", True)),
     ])
-    def test_login_tracking(self, _, executing_user: str, success: bool, expected_outcome):
+    def test_login_tracking_via_login(self, _, executing_user: str, success: bool, expected_outcome):
 
         user = self.fixture_manager.get_fixture('users', executing_user)
         n_attempts = 5
@@ -51,3 +54,34 @@ class TestLoginTracking(BaseIntegrationTest):
         db_user = self.fixture_manager.get_fixture_db("users", executing_user)
 
         assert (response.status_int, response.location.split("/")[-1], db_user.enabled) == expected_outcome
+
+    @parameterized.expand([
+        # TEST_NAME                    EXEC_USER   SUCCESS?  EXPECTED_OUTCOME
+        ("login_failure_with_block"  , "user_a"  , False   , (401, False)),
+        ("login_success_after_n-1"   , "user_a"  , True    , (200, True)),
+    ])
+    def test_login_tracking_via_apikey(self, _, executing_user: str, success: bool, expected_outcome):
+
+        n_attempts = 5
+        self.set_application_setting("security_max_failed_login_attempts", n_attempts)
+
+        for attempt in range(1, n_attempts + 1):
+            failed_attempt = attempt < n_attempts or not success
+            apikey = self.fixture_manager.get_fixture('apikeys', executing_user + ("_expired" if failed_attempt else ""))
+
+            self.testapp.get(
+                f"{base_url}/rpc/whoami",
+                headers=get_auth_header(apikey.value_plain),
+                status=401 if failed_attempt else 200
+            )
+
+        apikey = self.fixture_manager.get_fixture('apikeys', executing_user)
+        response = self.testapp.get(
+            f"{base_url}/rpc/whoami",
+            status=expected_outcome[0],
+            headers=get_auth_header(apikey.value_plain)
+        )
+
+        db_user = self.fixture_manager.get_fixture_db("users", executing_user)
+
+        assert (response.status_int, db_user.enabled) == expected_outcome
