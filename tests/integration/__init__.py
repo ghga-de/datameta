b9db6839a822b75b884ba5d4ef774a3a5b0d0def
Camill Kaipf <ckaipf@posteo.de>
2022-08-15 12:02:46 +0200

    try to create a 'utf8' DB first, if it fails create a 'SQL_ASCII' encoded

--- tests/integration/__init__.py
+++ tests/integration/__init__.py
@@ -6,7 +6,7 @@ import tempfile
 
 import transaction
 from sqlalchemy_utils import create_database, drop_database, database_exists
-
+from sqlalchemy.exc import DataError
 from datameta.models import (
     get_engine,
     get_session_factory,
@@ -38,7 +38,18 @@ class BaseIntegrationTest(unittest.TestCase):
         # create database from scratch:
         if database_exists(db_url):
             drop_database(db_url)
-        create_database(db_url)
+        
+        # handling encoding error
+        # (psycopg2.errors.InvalidParameterValue) new encoding (UTF8) 
+        # is incompatible with the encoding of the template database (SQL_ASCII)
+        try:
+            create_database(db_url, template="template1", encoding="utf8")
+        except DataError:
+            pass
+        try:
+            create_database(db_url, template="template1", encoding="SQL_ASCII")
+        except DataError:
+            print('Error while creating database (potentially an encoding problem)')
 
         # get engine and session factory
         self.engine = get_engine(self.settings)
